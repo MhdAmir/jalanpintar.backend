9cc27c652759bb365f1af8c9b1c778544b1212be
MhdAmir <m.amir.hs19@gmail.com>
2025-11-14 01:44:57 +0700

    fix: Correct whitespace in URL configuration for public and public_direct disks

--- config/filesystems.php
+++ config/filesystems.php
@@ -41,7 +41,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
@@ -51,7 +51,7 @@ return [
         'public_direct' => [
             'driver' => 'local',
             'root' => public_path('storage'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
